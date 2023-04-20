bf2384c96404140e245286b129007de3a1270997
Alexandra Antalová <alex.antalova@gmail.com>
2023-04-20 10:55:16 +0200

    Pridanie podmienok winner a vytvorenie playfield

--- hra.js
+++ hra.js
@@ -1,7 +1,10 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 
+/*
 const btnElm1 = document.querySelector('button:nth-child(1)');
-//console.log(btnElm1)
+console.log(btnElm1)
 const btnElm2 = document.querySelector('button:nth-child(2)');
 const btnElm3 = document.querySelector('button:nth-child(3)');
 const btnElm4 = document.querySelector('button:nth-child(4)');
@@ -11,22 +14,67 @@ const btnElm7 = document.querySelector('button:nth-child(7)');
 const btnElm8 = document.querySelector('button:nth-child(8)');
 const btnElm9 = document.querySelector('button:nth-child(9)');
 const btnElm10 = document.querySelector('button:nth-child(10)');
-
-
+*/
+//podmienka úkol 3
 const changeSign = (event) => {
   if (currentPlayer === 'circle') {
     currentPlayer = 'cross';
     event.target.classList.remove('board__field--cross');
     event.target.classList.add('board__field--circle');
+    document.getElementById('hrac').classList.remove('circle');
+    document.getElementById('hrac').classList.add('cross');
   } else {
     currentPlayer = 'circle';
     event.target.classList.remove('board__field--circle');
     event.target.classList.add('board__field--cross');
+    document.getElementById('hrac').classList.remove('cross');
+    document.getElementById('hrac').classList.add('circle');
   }
 
   event.target.disabled = true;
 };
 
+//vytvorenie poľa z main
+const playField = document.querySelectorAll('button');
+const playFieldArray = Array.from(playField);
+const Signs = playFieldArray.map((sign) => {
+  if (sign.classList.contains('board__field--cross')) {
+    return 'x';
+  } else if (sign.classList.contains('board__field--circle')) {
+    return 'o';
+  } else {
+    return '_';
+  }
+});
+
+
+
+const winner = findWinner(playFieldArray);
+if (winner === 'x') {
+  setTimeout(() => {
+    alert('Vyhrál křížik!');
+    location.reload();
+  }, 200);
+} else if (winner === 'o') {
+  setTimeout(() => {
+    alert('Vyhrálo kolečko!');
+    location.reload();
+  }, 200);
+} else if (winner === 'tie') {
+  setTimeout(() => {
+    alert('Hra skončila remízou.');
+    location.reload();
+  }, 200);
+}
+
+
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+  button.addEventListener('click', changeSign);
+});
+
+/*
 btnElm1.addEventListener('click', changeSign);
 btnElm2.addEventListener('click', changeSign);
 btnElm3.addEventListener('click', changeSign);
@@ -37,6 +85,7 @@ btnElm7.addEventListener('click', changeSign);
 btnElm8.addEventListener('click', changeSign);
 btnElm9.addEventListener('click', changeSign);
 btnElm10.addEventListener('click', changeSign);
+*/
 
 /*
 const restartElm = document.querySelector('.restart')
